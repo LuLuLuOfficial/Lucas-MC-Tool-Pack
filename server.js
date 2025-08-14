90f726e58e4d5ef9496814a8a4e83b249f669ef2
LuLuLuOfficial <xframilrainx@163.com>
2025-08-15 02:16:58 +0800

    主页修改和Node启动脚本修改

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ import express from "express";
 const PORT = 8080;
 
 const app = express();
+
 app.use(express.static("."));
 
-app.listen(PORT, () =>
-  console.log(`Listening on port http://127.0.0.1:${PORT}`)
-);
+app.use((req, res) => {
+  res.status(404).send("404 - File not found");
+});
+
+app.listen(PORT, '127.0.0.1', () => {
+  console.log(`✅ Server running at http://127.0.0.1:${PORT}`);
+});
